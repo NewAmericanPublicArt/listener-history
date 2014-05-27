d03f57e8380866d50caa51b312d4de9415070e8a
Brandon Stafford <brandon@pingswept.org>
2014-05-27 16:44:27 -0400

    Test code for motion sensor

--- motion/motion.ino
+++ motion/motion.ino
@@ -1,1 +1,19 @@
-# Code goes here
+int motion = 4;
+int led = 13;
+
+void setup() {
+	pinMode(motion, INPUT);
+	pinMode(led, OUTPUT);
+	Serial.begin(9600);
+}
+
+void loop() {
+	if(digitalRead(motion)) {
+		Serial.println("Found something!");
+		digitalWrite(led, HIGH);
+	} else {
+		Serial.println("Nothing");
+		digitalWrite(led, LOW);
+	}
+	delay(300);
+}
